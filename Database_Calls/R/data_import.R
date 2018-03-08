74582c3a44d7182a510ff445950739bcc1e2bff5
Rob Fowler <robgfowler@gmail.com>
2018-03-08 15:21:59 -0500

    Data Pull #2
    Pulled observations and tracks,
    determined tracks do not include observations
    Created on table with all data need to creae track fragments with feaures.
    
    Next steps
    - single feature per Effort, lat, lon.
    - create boundaries
    - asign attributes
    - calculate track frag properties
    --length
    --orientation wrt to ordering
    --order
    --bearing/ heading

--- Database_Calls/R/data_import.R
+++ Database_Calls/R/data_import.R
@@ -35,7 +35,7 @@ front_FlightCreew_id <- tbl(con, "FlightCrew") %>%
   collect()
 
 #Filter Effort Ids retain Seat info
-fltr_DesignFlown <- tbl(con, "DesignFlown") %>%
+vw_winter_front_DesignFlown <- tbl(con, "DesignFlown") %>%
   filter(DesignId %in% Winter_Design_id$DesignId) %>%
   filter(CrewMemberRoleId %in% front_FlightCreew_id$CrewMemberRoleId) %>%
   select(EffortId, DesignId, CrewMemberRoleId, dateFlown, ReplicateNumber) %>%
@@ -44,9 +44,39 @@ fltr_DesignFlown <- tbl(con, "DesignFlown") %>%
 
 
 # Relavent EffortId, DesignId, Seat Combination ----
-front_winter_effort <- fltr_DesignFlown %>%
+front_winter_effort <- vw_winter_front_DesignFlown %>%
   left_join(front_FlightCreew_id, by = "CrewMemberRoleId") %>%
   select(EffortId, DesignId, Seat)
 
 # Pull GpsTrack_point with above EffortId
-frnt_wntr_trck_pnts <- tbl(con, "Gps_Track_point")
+frnt_wntr_trck_pnts <- tbl(con, "GpsTrack_point") %>%
+  filter(EffortId %in% front_winter_effort$EffortId) %>%
+  select(EffortId, SecondsFromMidnight, longitude_dd, latitude_dd,
+         GpsError, PointType, ConditionCode) %>%
+  collect()
+
+# Pull from Observation table
+frnt_wntr_obs <- tbl(con, "Observation") %>%
+  filter(EffortId %in% front_winter_effort$EffortId) %>%
+  filter(SpeciesId %in% c("BLSC", "LTDU", "BUFF", "MERG",
+                          "RBME", "COGO", "SUSC", "SCOT",
+                          "DUCK", "WWSC", "COEI", "COME",
+                          "HOME", "BAGO", "GOLD", "DWSC",
+                          "GOME", "HARD", "UNDD", "EIDE")) %>%
+  select(ObservationId, EffortId, SpeciesId,
+         longitude_dd, latitude_dd, Count) %>%
+  collect()
+
+DBI::dbDisconnect(con)
+
+
+# Simplify to an observation and track view with Design and Effort ids
+vw_trk <- left_join(frnt_wntr_trck_pnts, front_winter_effort, by = "EffortId")
+vw_obs <- left_join(frnt_wntr_obs,       front_winter_effort, by = "EffortId")
+
+# I have determined that observations were not included as track points.
+# So... we need to join these views to get all our useful points in one place to be ordered.
+
+pre_track_frag <- full_join(vw_trk, vw_obs,
+                            by = c("EffortId", "longitude_dd",
+                                   "latitude_dd", "DesignId", "Seat"))
---
+++ Database_Calls/R/tmp.R
@@ -0,0 +1,2 @@
+#Recalling requested species
+(requested_species <- unique(Segment_seabird_lf_rf_effort$spc_id))
